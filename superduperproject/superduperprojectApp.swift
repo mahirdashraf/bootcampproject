515d571fbc1191b948c4f1f9d298d6e2eb1b695c
ashleyissupercool <ashleyani1012@gmail.com>
2026-03-16 19:00:36 -0400

    Initial Commit

---
+++ superduperproject/superduperprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  superduperprojectApp.swift
+//  superduperproject
+//
+//  Created by Ashley Ni on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct superduperprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
