0e406dab80b5665331bbbde69c2a458385815811
ashleyissupercool <ashleyani1012@gmail.com>
2026-04-14 17:46:34 -0400

    again

--- superduperproject/Models/Blindboxes.swift
+++ superduperproject/Models/Blindboxes.swift
@@ -1,1 +1,18 @@
+//
+//  Blindboxes.swift
+//
+//
+//  Created by Ashley Ni on 4/6/26.
+//
+
+import SwiftUI
+
+struct Blindboxes {
+    var title: String
+    var price: Int
+    var image: String
+    var character: Character
+}
+
+
 
--- superduperproject/Models/User.swift
+++ superduperproject/Models/User.swift
@@ -1,1 +1,17 @@
+//
+//  User.swift
+//
+//
+//  Created by Ashley Ni on 4/6/26.
+//
 
+struct User {
+    var moneyBalance: Double
+    var boxesOpened: Int
+    var characters: [Character]
+    func getCharacters() -> [Character] {
+        return characters
+    }
+}
+
+//testing
